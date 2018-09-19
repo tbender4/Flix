85a0cd61bef9df43260f43413b0f4d30742de67a
Kashfi Fahim <kashfi.fahim@gmail.com>
2018-09-19 16:57:57 -0400

    Create the NowPlayingVC class assign to initial view controller Create the MovieCell table view cell class and assign to cell class
    In code, uitableview data source set up with 2 mandatory functions

---
+++ Flix/Controller/NowPlayingVC.swift
@@ -0,0 +1,36 @@
+//
+//  NowPlayingVC.swift
+//  Flix
+//
+//  Created by iOS on 9/19/18.
+//  Copyright © 2018 Kashfi Fahim. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingVC: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        tableView.dataSource = self
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath)
+        return cell
+    }
+
+}
---
+++ Flix/View/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by iOS on 9/19/18.
+//  Copyright © 2018 Kashfi Fahim. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
