d1008198e91eeb45237e399e90cd849dadcde96d
Kashfi Fahim <kashfi.fahim@gmail.com>
2018-09-20 11:09:44 -0400

    Create network request, store json data into dataDictionary print out to console

--- Flix/Controller/NowPlayingVC.swift
+++ Flix/Controller/NowPlayingVC.swift
@@ -16,14 +16,25 @@ class NowPlayingVC: UIViewController {
     override func viewDidLoad() {
 //        tableView.dataSource = self
         super.viewDidLoad()
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                print(dataDictionary)
+            }
+            
+        }
+        task.resume()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
