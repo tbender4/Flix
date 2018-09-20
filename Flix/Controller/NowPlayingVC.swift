1e0135d716d8b089a4dc7370b733f0a93e44d78f
Kashfi Fahim <kashfi.fahim@gmail.com>
2018-09-20 17:57:09 -0400

    Pull to refresh

--- Flix/Controller/NowPlayingVC.swift
+++ Flix/Controller/NowPlayingVC.swift
@@ -14,15 +14,24 @@ class NowPlayingVC: UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
     var movies: [[String:Any]] = []
+    var refreshControl: UIRefreshControl!
     
-    
+
     override func viewDidLoad() {
-//        tableView.dataSource = self
         super.viewDidLoad()
-        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingVC.didPullToRefresh(_:)), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
         tableView.dataSource = self
-        
-        
+        fetchMovies()
+    
+    }
+    
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl){
+    self.fetchMovies()
+    }
+    
+    func fetchMovies() {
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -35,11 +44,11 @@ class NowPlayingVC: UIViewController, UITableViewDataSource {
                 let movies = dataDictionary["results"] as! [[String: Any]]
                 self.movies = movies
                 self.tableView.reloadData()
-                }
+                self.refreshControl.endRefreshing()
             }
-            task.resume()
-
-        // Do any additional setup after loading the view.
+        }
+        task.resume()
+        
     }
 
     
